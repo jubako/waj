c23db350caf33681e72294b474ef9ebc0413d29e
Matthieu Gautier <mgautier@kymeria.fr>
2023-10-12 15:01:47 +0200

    Better Entry.
    Yes, I'm lazy to create several commits :)

--- libwaj/src/create/entry.rs
+++ libwaj/src/create/entry.rs
@@ -6,6 +6,7 @@ pub struct Path1 {
     prefix: u8,
     size: u16,
 }
+static_assertions::assert_eq_size!(Path1, [u8; 24]);
 
 impl Path1 {
     pub fn new(mut path: Vec<u8>, value_store: &creator::StoreHandle) -> Self {
@@ -22,20 +23,13 @@ impl Path1 {
 }
 
 pub struct Content {
-    idx: Vow<EntryIdx>,
-    // The three path_* are technically a Path1.
-    // But extract the three fields from the Path1 allow compiler to
-    // reorganise the fields and reduce the structure size.
-    path_value_id: creator::ValueHandle,
-    path_prefix: u8,
-    path_size: u16,
     mimetype: creator::ValueHandle,
     content_id: ContentIdx,
     pack_id: PackId,
 }
-static_assertions::assert_eq_size!(Entry, [u8; 56]);
+static_assertions::assert_eq_size!(Content, [u8; 24]);
 
-pub struct Redirect {
+pub struct Entry {
     idx: Vow<EntryIdx>,
     // The three path_* are technically a Path1.
     // But extract the three fields from the Path1 allow compiler to
@@ -43,21 +37,15 @@ pub struct Redirect {
     path_value_id: creator::ValueHandle,
     path_prefix: u8,
     path_size: u16,
-    // The three target_* are technically a Path1.
-    // But extract the three fields from the Path1 allow compiler to
-    // reorganise the fields and reduce the structure size.
-    target_value_id: creator::ValueHandle,
-    target_prefix: u8,
-    target_size: u16,
+
+    kind: EntryKind,
 }
 
-static_assertions::assert_eq_size!(Redirect, [u8; 48]);
-
-pub enum Entry {
+pub enum EntryKind {
     Content(Content),
-    Redirect(Redirect),
+    Redirect(Path1),
 }
-static_assertions::assert_eq_size!(Entry, [u8; 56]);
+static_assertions::assert_eq_size!(Entry, [u8; 64]);
 
 impl Entry {
     pub fn new_content(
@@ -65,118 +53,112 @@ impl Entry {
         mimetype: creator::ValueHandle,
         content: ContentAddress,
     ) -> Self {
-        Self::Content(Content {
+        Self {
             idx: Vow::new(0.into()),
             path_value_id: path.value_id,
             path_prefix: path.prefix,
             path_size: path.size,
-            mimetype,
-            content_id: content.content_id,
-            pack_id: content.pack_id,
-        })
+            kind: EntryKind::Content(Content {
+                mimetype,
+                content_id: content.content_id,
+                pack_id: content.pack_id,
+            }),
+        }
     }
 
     pub fn new_redirect(path: Path1, target: Path1) -> Self {
-        Self::Redirect(Redirect {
+        Self {
             idx: Vow::new(0.into()),
             path_value_id: path.value_id,
             path_prefix: path.prefix,
             path_size: path.size,
-            target_value_id: target.value_id,
-            target_prefix: target.prefix,
-            target_size: target.size,
-        })
+
+            kind: EntryKind::Redirect(target),
+        }
     }
 }
 
 impl jubako::creator::EntryTrait<Property, EntryType> for Entry {
     fn variant_name(&self) -> Option<jubako::MayRef<EntryType>> {
-        Some(jubako::MayRef::Owned(match &self {
-            Self::Content(_) => EntryType::Content,
-            Self::Redirect(_) => EntryType::Redirect,
+        Some(jubako::MayRef::Owned(match self.kind {
+            EntryKind::Content(_) => EntryType::Content,
+            EntryKind::Redirect(_) => EntryType::Redirect,
         }))
     }
 
     fn value_count(&self) -> PropertyCount {
-        match &self {
-            Self::Content(_) => 3.into(),
-            Self::Redirect(_) => 2.into(),
+        match self.kind {
+            EntryKind::Content(_) => 3.into(),
+            EntryKind::Redirect(_) => 2.into(),
         }
     }
 
     fn set_idx(&mut self, idx: EntryIdx) {
-        match &self {
-            Self::Content(content) => content.idx.fulfil(idx),
-            Self::Redirect(redirect) => redirect.idx.fulfil(idx),
-        }
+        self.idx.fulfil(idx)
     }
 
     fn get_idx(&self) -> Bound<EntryIdx> {
-        match &self {
-            Self::Content(content) => content.idx.bind(),
-            Self::Redirect(redirect) => redirect.idx.bind(),
-        }
+        self.idx.bind()
     }
 
     fn value(&self, name: &Property) -> jubako::MayRef<creator::Value> {
-        jubako::MayRef::Owned(match &self {
-            Self::Content(content) => match name {
-                Property::Path => creator::Value::Array1(Box::new(creator::ArrayS::<1> {
-                    data: [content.path_prefix],
-                    value_id: content.path_value_id.clone_get(),
-                    size: content.path_size as usize,
-                })),
-                Property::Mimetype => {
+        jubako::MayRef::Owned(match name {
+            Property::Path => creator::Value::Array1(Box::new(creator::ArrayS::<1> {
+                data: [self.path_prefix],
+                value_id: self.path_value_id.clone_get(),
+                size: self.path_size as usize,
+            })),
+            Property::Mimetype => {
+                if let EntryKind::Content(content) = &self.kind {
                     creator::Value::IndirectArray(Box::new(content.mimetype.clone_get()))
+                } else {
+                    unreachable!()
                 }
-                Property::Content => creator::Value::Content(ContentAddress::new(
-                    content.pack_id,
-                    content.content_id,
-                )),
-                _ => unreachable!(),
-            },
-            Self::Redirect(redirect) => match name {
-                Property::Path => creator::Value::Array1(Box::new(creator::ArrayS::<1> {
-                    data: [redirect.path_prefix],
-                    value_id: redirect.path_value_id.clone_get(),
-                    size: redirect.path_size as usize,
-                })),
-                Property::Target => creator::Value::Array1(Box::new(creator::ArrayS::<1> {
-                    data: [redirect.target_prefix],
-                    value_id: redirect.target_value_id.clone_get(),
-                    size: redirect.target_size as usize,
-                })),
-                _ => unreachable!(),
-            },
+            }
+            Property::Content => {
+                if let EntryKind::Content(content) = &self.kind {
+                    creator::Value::Content(ContentAddress::new(
+                        content.pack_id,
+                        content.content_id,
+                    ))
+                } else {
+                    unreachable!()
+                }
+            }
+            Property::Target => {
+                if let EntryKind::Redirect(target) = &self.kind {
+                    creator::Value::Array1(Box::new(creator::ArrayS::<1> {
+                        data: [target.prefix],
+                        value_id: target.value_id.clone_get(),
+                        size: target.size as usize,
+                    }))
+                } else {
+                    unreachable!()
+                }
+            }
         })
     }
 }
 
 impl jubako::creator::FullEntryTrait<Property, EntryType> for Entry {
-    fn compare<'i, I>(&self, sort_keys: &'i I, other: &Self) -> std::cmp::Ordering
+    fn compare<'i, I>(&self, _sort_keys: &'i I, other: &Self) -> std::cmp::Ordering
     where
         I: IntoIterator<Item = &'i Property> + Copy,
     {
         use std::cmp;
-        let mut iter = sort_keys.into_iter();
-        assert_eq!(iter.next(), Some(&Property::Path));
-        assert_eq!(iter.next(), None);
-        let (entry_prefix, entry_size, entry_value_id) = match self {
-            Self::Content(e) => (&e.path_prefix, &e.path_size, &e.path_value_id),
-            Self::Redirect(e) => (&e.path_prefix, &e.path_size, &e.path_value_id),
-        };
-        let (other_prefix, other_size, other_value_id) = match &other {
-            Self::Content(e) => (&e.path_prefix, &e.path_size, &e.path_value_id),
-            Self::Redirect(e) => (&e.path_prefix, &e.path_size, &e.path_value_id),
-        };
-        match entry_prefix.cmp(other_prefix) {
+        //let mut iter = sort_keys.into_iter();
+        //assert_eq!(iter.next(), Some(&Property::Path));
+        //assert_eq!(iter.next(), None);
+        match self.path_prefix.cmp(&other.path_prefix) {
             cmp::Ordering::Less => cmp::Ordering::Less,
             cmp::Ordering::Greater => cmp::Ordering::Greater,
-            cmp::Ordering::Equal => match entry_value_id.get().cmp(&other_value_id.get()) {
-                cmp::Ordering::Less => cmp::Ordering::Less,
-                cmp::Ordering::Greater => cmp::Ordering::Greater,
-                cmp::Ordering::Equal => entry_size.cmp(other_size),
-            },
+            cmp::Ordering::Equal => {
+                match self.path_value_id.get().cmp(&other.path_value_id.get()) {
+                    cmp::Ordering::Less => cmp::Ordering::Less,
+                    cmp::Ordering::Greater => cmp::Ordering::Greater,
+                    cmp::Ordering::Equal => self.path_size.cmp(&other.path_size),
+                }
+            }
         }
     }
 }
